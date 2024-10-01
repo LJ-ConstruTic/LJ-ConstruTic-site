c8c44c3cae7b04e180da7dde5e7395194e093e3b
Eurekamuteka <eureka@ljconstrutic.com>
2024-10-01 17:50:15 +0100

    style: add responsiveness in home page

--- src/app/[locale]/(pages)/services/[id]/page.tsx
+++ src/app/[locale]/(pages)/services/[id]/page.tsx
@@ -1,15 +1,15 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 
-export default function ProductPage({ params }: { params: { id: string } }) {
+export default function ServicePage({ params }: { params: { id: string } }) {
     return (
         <main className="w-full min-h-screen bg-gray-50 dark:bg-transparent">
             <section className="max-w-[1248px] px-3 xl:px-0 h-auto py-5 xl:py-0 xl:h-[573px] mx-auto w-full flex flex-col items-center gap-10 lg:flex-row ">
                 <div className="max-w-[540px] rounded-[14px] h-[473px] w-full bg-black"></div>
                 <div className="w-full flex flex-col gap-10">
                     <div>
-                        <h3 className="text-primary-blue">Nossos Produtos</h3>
-                        <h2 className="font-bold text-2xl">Produto {params.id}</h2>
+                        <h3 className="text-primary-blue">Nossos Serviços</h3>
+                        <h2 className="font-bold text-2xl">Serviço {params.id}</h2>
                     </div>
                     <div className="flex flex-col gap-6 text-sm">
                         <p>
--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -3,25 +3,41 @@ import { Button } from "../ui/button";
 import { Container } from "../ContainerRoot";
 
 export const Banner = () => {
+    const handleClick = (e: React.MouseEvent<HTMLAnchorElement>, targetId: string) => {
+        e.preventDefault();
+
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            setTimeout(() => {
+                targetElement.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start",
+                });
+            }, 200);
+        }
+    };
+
     return (
         <Container>
-            <section className="w-full flex mt-5 items-center">
+            <section className="w-full flex xl:flex-row flex-col mt-5 items-center">
                 <div className="w-full flex flex-col gap-9">
-                    <h4 className="text-[52px] font-bold leading-[65.35px]">
+                    <h4 className="lg:text-[52px] text-4xl sm:text-5xl font-bold sm:leading-[50px] xl:leading-[65.35px]">
                         Qualidade e eficiência <br /> em um só lugar
                     </h4>
-                    <p className="text-base max-w-[504px]">
+                    <p className="text-sm text-left lg:text-start lg:text-base max-w-[504px]">
                         Realizamos o sonho de todos de ter uma construção digna e moderna, ou um sistema inteligente que ajude na gestão do
                         dia a dia, transformando esse sonho em um direito para todos
                     </p>
                     <div>
-                        <Button className="bg-primary-blue dark:text-white flex items-center">
-                            <span>Contacte-nos agora</span>
-                            <ArrowRight />
-                        </Button>
+                        <a href="#contact" onClick={(e) => handleClick(e, "contact")}>
+                            <Button className="bg-primary-blue dark:text-white flex gap-1 items-center">
+                                <span>Contacte-nos agora</span>
+                                <ArrowRight width={16} />
+                            </Button>
+                        </a>
                     </div>
                 </div>
-                <div className="w-full h-[500px] bg-black rounded-[14px]"></div>
+                <div className="w-full h-[500px] xl:flex hidden bg-black rounded-[14px]"></div>
             </section>
         </Container>
     );
